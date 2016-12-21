f2c0c2ee594a28844a136676cc35e47aba18fa3a
sarkany adrian <adrian.sarkany@clever-software-solutions.com>
2016-12-21 14:43:15 +0200

    Contact View

--- src/routes/contact/Contact.js
+++ src/routes/contact/Contact.js
@@ -1,11 +1,18 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Contact.css';
+import ContactForm from '../../components/ContactForm';
 
 class Contact extends React.Component {
   render() {
     return (
-      <div className={s.root}>HOME</div>
+      <div className={s.contactpage}>
+        <div className={s.pagewrapper}>
+          <div className={s.formwrapper}>
+            <ContactForm />
+          </div>
+        </div>
+      </div>
     );
   }
 }
