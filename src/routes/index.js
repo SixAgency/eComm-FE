d63d6699882b5af99ea9bb8390e2fe509d36ca48
leventecsordas <levente.csordas@clever-software-solutions.com>
2017-01-03 18:59:03 +0200

    Refactor - Routing Part 1

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,39 +1,196 @@
-/**
- * React Starter Kit (https://www.reactstarterkit.com/)
- *
- * Copyright © 2014-2016 Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
+import React from 'react';
+import fetch from '../core/fetch';
+import Layout from '../components/Layout';
+// Pages
+import Home from './home/Home';
+import Account from './account/Account';
+import Biography from './biography/Biography';
+import Cart from './cart/Cart';
+import Checkout from './checkout/Checkout';
+import Product from './product/Product';
+import ProductCategory from './productCategory/ProductCategory';
+import Contact from './contact/Contact';
+import NotFound from './notFound/NotFound';
 
-/* eslint-disable global-require */
-
-// The top-level (parent) route
-export default {
-
+const routes = {
   path: '/',
-
-  // Keep in mind, routes are evaluated in order
   children: [
-    require('./home').default,
-    require('./biography').default,
-    require('./account').default,
-    require('./cart').default,
-    require('./contact').default,
-    require('./product').default,
-    require('./checkout').default,
-    require('./productCategory').default,
-    require('./notFound').default,
+    {
+      path: '/',
+      async action() {
+        // TODO - ERROR HANDLING
+        const products = await fetch('/api/products', { credentials: 'same-origin' })
+          .then((resp) => (resp.json())
+            .then((json) => (json.products)));
+        return {
+          headerClass: 'default',
+          activeSlug: '/',
+          title: 'Shop',
+          content: <Home gridItems={products} />,
+        };
+      },
+    },
+    {
+      path: '/biography',
+      action() {
+        return {
+          headerClass: 'default',
+          activeSlug: '/biography',
+          title: 'Biography',
+          content: <Biography />,
+        };
+      },
+    },
+    {
+      path: '/checkout',
+      async action() {
+        const user = await fetch('/api/check', { credentials: 'same-origin' })
+          .then((resp) => (resp.json())
+            .then((json) => (json)));
+        const cart = await fetch('/api/cart', { credentials: 'same-origin' })
+        .then((resp) => (resp.json())
+          .then((json) => (json)));
+        return {
+          headerClass: 'colored',
+          activeSlug: '/',
+          title: 'Checkout',
+          content: <Checkout {...user} cartItems={cart} />,
+        };
+      },
+    },
+    {
+      path: '/cart',
+      async action() {
+        const cart = await fetch('/api/cart', { credentials: 'same-origin' })
+          .then((resp) => (resp.json())
+            .then((json) => (json)));
+        return {
+          headerClass: 'default',
+          activeSlug: '/',
+          title: 'Cart',
+          content: <Cart cartItems={cart} />,
+        };
+      },
+    },
+    {
+      path: '/contact',
+      action() {
+        return {
+          headerClass: 'colored',
+          activeSlug: '/contact',
+          title: 'Contact',
+          content: <Contact />,
+        };
+      },
+    },
+    {
+      path: '/product/:slug',
+      async action(context) {
+        const slug = context.params.slug;
+        // TODO - ERROR HANDLING
+        const [product] = await Promise.all([
+          fetch(`/api/products/${slug}`, { credentials: 'same-origin' })
+          .then((resp) => (resp.json())
+            .then((json) => (json))),
+        ]);
+        if (product) {
+          return {
+            headerClass: 'colored',
+            activeSlug: '/',
+            title: product.name || 'Shop',
+            content: <Product product={product} />,
+          };
+        }
+        return {
+          title: 'Shop',
+          content: null,
+        };
+      },
+    },
+    {
+      path: '/product-category/:category',
+      async action(context) {
+        const category = context.params.category;
+        console.log(category);
+        // TODO - ERROR HANDLING
+        const products = await fetch('/api/products', { credentials: 'same-origin' })
+          .then((resp) => (resp.json())
+            .then((json) => (json.products)));
+        return {
+          headerClass: 'default',
+          activeSlug: '/',
+          title: 'Archives',
+          content: <ProductCategory products={products} />,
+        };
+      },
+    },
+    {
+      path: '/my-account',
+      children: [
+        {
+          path: '/',
+          async action() {
+            const user = await fetch('/api/check', { credentials: 'same-origin' })
+              .then((resp) => (resp.json())
+                .then((json) => (json)));
+            return {
+              headerClass: 'colored',
+              activeSlug: '/my-account',
+              title: 'My Account',
+              content: <Account {...user} />,
+            };
+          },
+        },
+        {
+          path: '/login',
+          action() {
+            return {
+              title: 'My Account',
+              content: 'Login',
+            };
+          },
+        },
+        {
+          path: '/register',
+          action() {
+            return {
+              title: 'My Account',
+              content: 'Register',
+            };
+          },
+        },
+      ],
+    },
+    {
+      path: '*',
+      action() {
+        const title = 'Page Not Found';
+        return {
+          headerClass: 'default',
+          activeSlug: '/',
+          title,
+          content: <NotFound title={title} />,
+          status: 404,
+        };
+      },
+    },
   ],
   async action({ next }) {
     // Execute each child route until one of them return the result
     const route = await next();
-
+    const cart = await fetch('/api/cart', { credentials: 'same-origin' })
+      .then((resp) => (resp.json())
+        .then((json) => (json)));
     // Provide default values for title, description etc.
     route.title = `${route.title || 'Untitled Page'} - krissorbie`;
     route.description = route.description || '';
+    route.component = (
+      <Layout headerClass={route.headerClass} activeSlug={route.activeSlug} cartItems={cart}>
+        {route.content}
+      </Layout>
+    );
     return route;
   },
-
 };
+
+export default routes;
