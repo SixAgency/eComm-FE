876e8deae942002c4f6fac51cb7bd5553e3e3925
sarkany adrian <adrian.sarkany@clever-software-solutions.com>
2017-01-04 18:17:04 +0200

    Small Fixes for links

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Dashboard.css';
+import Link from '../Link';
 
 class Dashboard extends React.Component {
   static propTypes = {
@@ -16,27 +17,27 @@ class Dashboard extends React.Component {
          <a href="" className={s.actions}> Sign out</a>).
          From your account dashboard you can view your recent orders, manage your
          shipping and billing addresses and
-          <a href="/edit-account" className={s.actions}> edit your password and account details</a>.
+          <Link className={s.actions} to="/edit-account" > edit your password and account details </Link>.
         </p>
         <h1 className={s.title}>MY ADDRESSES</h1>
         <p className={s.info}>The following addresses will be used on the checkout page.</p>
         <div className={s.addressescont}>
           <div className={s.addresseswrpr}>
-            <a
-              href="/edit-billing-address"
+            <Link
               className={s.options}
+              to="/edit-billing-address"
             >
               Edit billing address
-            </a>
+            </Link>
             <p className={s.optiontext}> Please set up your billing address </p>
           </div>
           <div className={s.addresseswrpr}>
-            <a
-              href="/edit-shipping-address"
+            <Link
               className={s.options}
+              to="/edit-shipping-address"
             >
             Edit shipping address
-            </a>
+            </Link>
             <p className={s.optiontext}> Please set up your shipping address </p>
           </div>
         </div>
