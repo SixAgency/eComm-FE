337831b43ae10312bf56a434e9cc473385ed4783
sarkany adrian <adrian.sarkany@clever-software-solutions.com>
2017-01-09 13:55:24 +0200

    Product Grid Actions

---
+++ src/components/ProductGridItem/ProductAction/ProductAction.js
@@ -0,0 +1,27 @@
+import React, { PropTypes } from 'react';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import cx from 'classnames';
+import Link from '../../Link';
+import s from './../ProductGridItem.css';
+
+class ProductAction extends React.Component {
+  static propTypes = {
+    text: PropTypes.string.isRequired,
+    link: PropTypes.string.isRequired,
+    action: PropTypes.func,
+  }
+
+  render() {
+    return (
+      <Link
+        className={cx(s.button, s.select)}
+        to={this.props.link}
+        onClick={this.props.action}
+      >
+        {this.props.text}
+      </Link>
+    );
+  }
+}
+
+export default withStyles(s)(ProductAction);
--- src/components/ProductGridItem/ProductGridItem.js
+++ src/components/ProductGridItem/ProductGridItem.js
@@ -3,6 +3,7 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import cx from 'classnames';
 import s from './ProductGridItem.css';
 import Link from '../Link';
+import ProductAction from './ProductAction';
 
 class ProductGridItem extends React.Component {
   static propTypes = {
@@ -12,9 +13,38 @@ class ProductGridItem extends React.Component {
     catclass: PropTypes.string,
   }
 
-  render() {
+  addToCart = () => {
+    console.log('added to cart');
+  }
+
+  handleText = () => {
     const product = this.props.product;
 
+    if (product.has_variants) {
+      return {
+        text: (product.classifications[0].taxon.name === 'Gifts' ? 'Select Amount' : 'Select Options'),
+        link: `/product/${product.slug}`,
+      };
+    }
+    return {
+      text: 'Buy',
+      link: '/cart',
+      action: this.addToCart,
+    };
+  }
+
+  handlePricing = () => {
+    const product = this.props.product;
+    if (product.classifications[0].taxon.name === 'Gifts') {
+      return `${product.display_price} - ${product.variants.slice(-1)[0].price}`;
+    }
+    return product.display_price;
+  }
+
+  render() {
+    const product = this.props.product;
+    const actionProperties = this.handleText();
+    const price = this.handlePricing();
     return (
       <div className={s.productgrid}>
         <Link className={s.plink} to={`/product/${product.slug}`}>
@@ -24,14 +54,20 @@ class ProductGridItem extends React.Component {
           <div className={s.itemmeta}>
             <Link className={s.plink} to={`/product/${product.slug}`}>
               <span className={cx(s.pprice, s[this.props.priceclass])}>
-                {product.display_price}–{product.display_price}
+                {price}
               </span>
               <h2 className={cx(s.pname, s[this.props.nameclass])}>{product.name}</h2>
-              <h5 className={cx(s.pcat, s[this.props.catclass])}>GIFTS</h5>
+              <h5 className={cx(s.pcat, s[this.props.catclass])}>
+                {this.props.product.classifications[0].taxon.name}
+              </h5>
             </Link>
             <div className={s.buttons}>
               <Link className={cx(s.button, s.view)} to={`/product/${product.slug}`}>View</Link>
-              <Link className={cx(s.button, s.select)} to={`/product/${product.slug}`}>Select amount</Link>
+              <ProductAction
+                text={actionProperties.text}
+                link={actionProperties.link}
+                action={actionProperties.action}
+              />
             </div>
           </div>
         </div>
